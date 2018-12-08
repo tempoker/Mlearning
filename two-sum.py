ad917dfb5124cd2e4861dcdfb9f8381c37440c97
tempoker <1578852802@qq.com>
2018-12-08 23:36:56 +0800

    add a new factor

--- two-sum.py
+++ two-sum.py
@@ -12,7 +12,7 @@ class Solution(object):
             d[num] = i
         # no special case handling becasue it's assumed that it has only one solution
             
-la = [2,5,9,11,6,12]
+la = [2,5,9,11,6,12,13,14]
 target = 8
 aa = Solution()
 res = aa.twoSum(la,target)

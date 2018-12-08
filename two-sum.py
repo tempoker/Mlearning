eac36a3a45f414cf08dcdd53963b3b7a7821beaa
tempoker <1578852802@qq.com>
2018-12-08 23:46:10 +0800

    add a new factor

--- two-sum.py
+++ two-sum.py
@@ -10,10 +10,12 @@ class Solution(object):
             if target - num in d:
                 return [d[target - num], i]
             d[num] = i
+        else :
+            return False
         # no special case handling becasue it's assumed that it has only one solution
             
-la = [2,5,9,11,6,12,13,14]
-target = 8
+la = [2,5,9,11,6,12]
+target = 17
 aa = Solution()
 res = aa.twoSum(la,target)
 print(res) #[1,3]

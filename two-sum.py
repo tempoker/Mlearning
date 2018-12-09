48513e5fae00c2ded7cb44212ce5ff0dcfd70d5e
tempoker <1578852802@qq.com>
2018-12-09 23:45:07 +0800

    there is a file of kinf of pkl

--- two-sum.py
+++ two-sum.py
@@ -1,21 +1,27 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        d = {}
-        for i, num in enumerate(nums):
-            if target - num in d:
-                return [d[target - num], i]
-            d[num] = i
-        else :
-            return False
+
+import pickle as pkl 
+#class Solution(object):
+def twoSum(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    d = {}
+    for i, num in enumerate(nums):
+        if target - num in d:
+            return [d[target - num], i]
+        d[num] = i
+    else :
+        return False
         # no special case handling becasue it's assumed that it has only one solution
-            
+
+
 la = [2,5,9,11,6,12]
-target = 17
-aa = Solution()
-res = aa.twoSum(la,target)
+target = 18
+# with open(r'tsum.pkl','wb') as f:
+#     pkl.dump(twoSum,f)
+with open(r'tsum.pkl','rb') as f:
+    twoSum = pkl.load(f)
+res = twoSum(la,target)
 print(res) #[1,3]
